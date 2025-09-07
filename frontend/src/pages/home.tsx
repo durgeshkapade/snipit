175086701c2d0b1218f69f255364087a9cb5292e
udaybhartiya626 <jaybalaji192@gmail.com>
2025-09-07 19:06:22 +0530

    feat: button added in home

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button"
 import Header from "@/components/ui/header"
 import { Textarea } from "@/components/ui/textarea"
 
@@ -6,9 +7,15 @@ export const HomePage = () => {
     return (
         <div className="h-full ">
             <Header />
-            <Textarea name="" id="" placeholder="Enter your snippet here..." className="h-1/2 w-[95%] mx-auto">
-
-            </Textarea>
+            <div className="m-5 h-[75%]">
+                <Textarea placeholder="Enter your snippet here..." className="h-full w-full mx-auto">
+                </Textarea>
+            </div>
+            <div className="w-full h-fit flex justify-end px-5">
+                <Button>
+                    Paste
+                </Button>
+            </div>
         </div>
     )
 }
