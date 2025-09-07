768cbb82ca82801d4d3aa45147d0cd91bcc55e89
udaybhartiya626 <jaybalaji192@gmail.com>
2025-09-07 18:50:55 +0530

    style: text area added

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,10 +1,14 @@
 import Header from "@/components/ui/header"
+import { Textarea } from "@/components/ui/textarea"
 
 
 export const HomePage = () => {
     return (
-        <div>
+        <div className="h-full ">
             <Header />
+            <Textarea name="" id="" placeholder="Enter your snippet here..." className="h-1/2 w-[95%] mx-auto">
+
+            </Textarea>
         </div>
     )
 }
