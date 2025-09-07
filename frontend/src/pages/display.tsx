4cd1ff6e46b63bd59dbaa8b3ce1ba94b22f9a788
durgeshkapade <kapadedurgesh6@gmail.com>
2025-09-07 19:05:35 +0530

    feat: code Display

--- frontend/src/pages/display.tsx
+++ frontend/src/pages/display.tsx
@@ -1,7 +1,49 @@
+import { Button } from "@/components/ui/button"
+import Header from "@/components/ui/header"
+import { Textarea } from "@/components/ui/textarea"
 
 const DisplayPage = () => {
   return (
-    <div>DisplayPage</div>
+    <div className="h-full">
+
+      {/* // header */}
+
+      <Header />
+
+      <div className="flex justify-between flex-col ">
+
+          {/* // Button */}
+          <div className="bg-amber-500 px-2 py-3 ">
+            <Button className="mr-2" onClick={()=>{
+
+            }}>
+              Edit 
+            </Button>
+
+            <Button onClick={()=>{
+
+            }}>
+              Delete 
+            </Button>
+          </div>
+
+          
+
+      </div>
+
+
+
+      {/* // Text Area */}
+    
+      <div className=" w-screen h-[80%] px-2 py-2 overflow-x-hidden ">
+        
+        <Textarea className="h-full" />
+
+      </div>
+
+      
+      
+    </div>
   )
 }
 
