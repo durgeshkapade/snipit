ba36cc0d8d55acfa8b62bceb0409515c3ea8c9c0
udaybhartiya626 <jaybalaji192@gmail.com>
2025-09-07 18:38:54 +0530

    style: update header component styling and structure

--- frontend/src/components/ui/header.tsx
+++ frontend/src/components/ui/header.tsx
@@ -1,17 +1,22 @@
+import { Link } from "react-router-dom"
+import { Button } from "./button"
 
 const Header = () => {
     return (
-        <header className='flex justify-between h-10 p-3 px-6'>
-            <h1>
-                Sniput
+        <header className='flex justify-between h-fit p-3 px-6 border shadow'>
+            <h1
+                className="text-5xl font-extrabold             tracking-tight             text-transparent             bg-clip-text             bg-gradient-to-r             from-purple-400             via-pink-500             to-red-500            transform             transition-transform             duration-300             ease-in-out             group-hover:scale-105          "            >
+                Snipit
             </h1>
             <div className='flex gap-2'>
-                <nav>
+                <Button variant={"ghost"}>
                     About
-                </nav>
-                <nav>
-                    History
-                </nav>
+                </Button>
+                <Link to={'/history'}>
+                    <Button variant={"ghost"}>
+                        History
+                    </Button>
+                </Link>
             </div>
         </header>
     )
