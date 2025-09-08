4c9081e665ce24b277c08bcd09152e7e4c815669
tejas <jaybalaji192@gmail.com>
2025-09-08 12:46:31 +0530

    feat: add configurations and cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,15 +2,24 @@ import express, { type Request, type Response } from 'express';
 const app = express();
 import { connectDB } from '@/config/db.js';
 import pasteRouter from './routes/paste.route.js';
+import cors from "cors"
+import configurations from './config/configurations.js';
+
 
 connectDB();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.get('/hello', (req: Request, res: Response) => {
     res.send("Hello")
 });
 
 app.use(express.json())
+    .use(cors(
+        {
+            origin:[configurations.frontend_url!],
+            credentials:true
+        }
+    ))
     .use('/api/', pasteRouter)
 
 
