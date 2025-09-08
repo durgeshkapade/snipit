40aeddf44e6621263549241fc5802bf707b84e36
tejas <jaybalaji192@gmail.com>
2025-09-08 17:16:36 +0530

    feat: implemented error and loading

--- frontend/src/pages/display.tsx
+++ frontend/src/pages/display.tsx
@@ -5,6 +5,7 @@ import { Textarea } from "@/components/ui/textarea";
 import { useApiHelpers } from "@/lib/api";
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import Error from "./error";
 
 const DisplayPage = () => {
   const { id } = useParams();
@@ -12,54 +13,69 @@ const DisplayPage = () => {
   const apiHelpers = useApiHelpers();
 
   const [isEdit, setIsEdit] = useState<boolean>(false);
-  const [content, setContent] = useState<string>("");
+  const [content, setContent] = useState<string>();
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
-    apiHelpers.getPaste(id!).then((data) => {
-      setContent(data.content);
-    });
+    async function loadData() {
+      const data = await apiHelpers.getPaste(id!);
+      if (data) setContent(data.content);
+      setLoading(false);
+    }
+    loadData();
   }, []);
 
   const handleDelete = async () => {
     const data = await apiHelpers.deletePaste(id!);
     if (data) navigate("/");
   };
+
   return (
     <div className="h-full">
-      <Header />
-      <div className="flex justify-between flex-col">
-        <div className="flex gap-3 px-2 py-3 ">
-          <Button
-            variant={"link"}
-            disabled={isEdit}
-            className="underline hover:text-blue-500"
-            onClick={() => setIsEdit(true)}
-          >
-            Edit
-          </Button>
+      <Header className="h-[10%]" />
 
-          <Button
-            variant={"link"}
-            className="underline hover:text-blue-500"
-            onClick={handleDelete}
-          >
-            Delete
-          </Button>
+      {loading ? (
+        <div className="flex items-center justify-center h-[90%]">
+          Loading... ⏳
         </div>
-      </div>
-      <div className=" w-screen h-[75%] px-6 py-2 overflow-x-hidden ">
-        {isEdit ? (
-          <Textarea className="h-full" />
-        ) : (
-          <Card className="h-full overflow-y-scroll ">
-            <CardContent className="h-fit">{content}</CardContent>
-          </Card>
-        )}
-      </div>
-      {isEdit && (
-        <div className="flex w-full h-fit justify-end px-6">
-          <Button onClick={() => setIsEdit(false)}>Save Changes</Button>
-        </div>
+      ) : content ? (
+        <>
+          <div className="flex justify-between flex-col">
+            <div className="flex gap-3 px-2 py-3 ">
+              <Button
+                variant={"link"}
+                disabled={isEdit}
+                className="underline hover:text-blue-500"
+                onClick={() => setIsEdit(true)}
+              >
+                Edit
+              </Button>
+              <Button
+                variant={"link"}
+                className="underline hover:text-blue-500"
+                onClick={handleDelete}
+              >
+                Delete
+              </Button>
+            </div>
+          </div>
+          <div className="w-screen h-[75%] px-6 py-2 overflow-x-hidden">
+            {isEdit ? (
+              <Textarea className="h-full" />
+            ) : (
+              <Card className="h-full overflow-y-scroll">
+                <CardContent className="h-fit">{content}</CardContent>
+              </Card>
+            )}
+          </div>
+          {isEdit && (
+            <div className="flex w-full h-fit justify-end px-6">
+              <Button onClick={() => setIsEdit(false)}>Save Changes</Button>
+            </div>
+          )}
+        </>
+      ) : (
+        <Error />
       )}
     </div>
   );
