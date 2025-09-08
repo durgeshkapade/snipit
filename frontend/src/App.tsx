557d752f36c40baa69db0fa1082e5d3a6e6537e3
tejas <jaybalaji192@gmail.com>
2025-09-08 11:50:39 +0530

    feat: implmented class and objects for services and controller

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,21 +6,17 @@ import HistoryPage from "@/pages/history";
 
 
 const App = () => {
-  return (
+    return (
         <div className='h-screen w-screen m-0 p-0 box-border'>
-
-          <Router>
-            <Routes>
-              <Route path="/" element={< HomePage/>} />
-              <Route path="/:id" element={<DisplayPage/>} />
-              <Route path="/history" element={< HistoryPage/>} />
-            </Routes>
-
-          </Router>
-
-
+            <Router>
+                <Routes>
+                    <Route path="/" element={< HomePage />} />
+                    <Route path="/:id" element={<DisplayPage />} />
+                    <Route path="/history" element={< HistoryPage />} />
+                </Routes>
+            </Router>
         </div>
-  )
+    )
 }
 
 export default App
