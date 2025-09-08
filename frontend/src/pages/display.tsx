84542b663094b7e949e4a96cbbf7459ce6ee1f56
tejas <jaybalaji192@gmail.com>
2025-09-08 15:53:01 +0530

    style: format

--- frontend/src/pages/display.tsx
+++ frontend/src/pages/display.tsx
@@ -1,73 +1,71 @@
-import { Button } from "@/components/ui/button"
-import { Card, CardContent } from "@/components/ui/card"
-import Header from "@/components/ui/header"
-import { Textarea } from "@/components/ui/textarea"
-import { useApiHelpers } from "@/lib/api"
-import { useEffect, useState } from "react"
-import { useNavigate, useParams } from "react-router-dom"
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import Header from "@/components/ui/header";
+import { Textarea } from "@/components/ui/textarea";
+import { useApiHelpers } from "@/lib/api";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
 const DisplayPage = () => {
-    const { id } = useParams();
-    const navigate = useNavigate();
-    const apiHelpers = useApiHelpers();
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const apiHelpers = useApiHelpers();
 
-    const [isEdit, setEdit] = useState<boolean>(false);
-    const [content, setContent] = useState<string>("");
+  const [isEdit, setEdit] = useState<boolean>(false);
+  const [content, setContent] = useState<string>("");
 
-    useEffect(() => {
-        apiHelpers.getPaste(id!).then((data) => {
-            setContent(data.content)
-        })
-    }, [])
+  useEffect(() => {
+    apiHelpers.getPaste(id!).then((data) => {
+      setContent(data.content);
+    });
+  }, []);
 
-    const handleDelete = async () => {
-        const data = await apiHelpers.deletePaste(id!)
-        if (data)
-            navigate('/')
-    }
+  const handleDelete = async () => {
+    const data = await apiHelpers.deletePaste(id!);
+    if (data) navigate("/");
+  };
+  return (
+    <div className="h-full">
+      <Header />
+      <div className="flex justify-between flex-col">
+        <div className="flex gap-3 px-2 py-3 ">
+          <Button
+            variant={"link"}
+            disabled={isEdit}
+            className="underline hover:text-blue-500"
+            onClick={() => setEdit(true)}
+          >
+            Edit
+          </Button>
 
-    return (
-        <div className="h-full">
-            <Header />
-            <div className="flex justify-between flex-col">
-                <div className="flex gap-3 px-2 py-3 ">
-                    <Button
-                        variant={"link"}
-                        disabled={isEdit}
-                        className="underline hover:text-blue-500"
-                        onClick={() => setEdit(true)}
-                    >
-                        Edit
-                    </Button>
-
-                    <Button variant={"link"} className="underline hover:text-blue-500"
-                        onClick={handleDelete}
-                    >
-                        Delete
-                    </Button>
-                </div>
-            </div>
-            <div className=" w-screen h-[75%] px-6 py-2 overflow-x-hidden ">
-                {isEdit ? <Textarea className="h-full" />
-                    :
-                    <Card className="h-full overflow-y-scroll ">
-                        <CardContent className="h-fit">
-                            {content}
-                        </CardContent>
-
-                    </Card>}
-            </div>
-            {isEdit && (
-                <div className="flex w-full h-fit justify-end px-6"
-                    onClick={() => setEdit(false)}
-                >
-                    <Button>
-                        Save Changes
-                    </Button>
-                </div>
-            )}
+          <Button
+            variant={"link"}
+            className="underline hover:text-blue-500"
+            onClick={handleDelete}
+          >
+            Delete
+          </Button>
         </div>
-    )
-}
+      </div>
+      <div className=" w-screen h-[75%] px-6 py-2 overflow-x-hidden ">
+        {isEdit ? (
+          <Textarea className="h-full" />
+        ) : (
+          <Card className="h-full overflow-y-scroll ">
+            <CardContent className="h-fit">{content}</CardContent>
+          </Card>
+        )}
+      </div>
+      {isEdit && (
+        <div
+          className="flex w-full h-fit justify-end px-6"
+          onClick={() => setEdit(false)}
+        >
+          <Button>Save Changes</Button>
+        </div>
+      )}
+    </div>
+  );
+};
 
-export default DisplayPage
+export default DisplayPage;
