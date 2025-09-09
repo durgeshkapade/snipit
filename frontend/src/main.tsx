ef4ff1a634f93acc060c66765c775622ac748818
tejas <jaybalaji192@gmail.com>
2025-09-09 19:15:16 +0530

    feat: toast changes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,19 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { Toaster } from "@/components/ui/sonner"
+import { Toaster } from "@/components/ui/sonner";
 import "./index.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Toaster 
+    <Toaster
       position="top-right"
       theme="dark"
       richColors
       duration={3000}
       // closeButton
       toastOptions={{ className: "p-4 rounded-lg" }}
-    />  
+    />
     <App />
   </StrictMode>,
 );
