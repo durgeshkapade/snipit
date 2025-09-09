6d7c7658aeca9f6040bd6ee15f7b109ca52095bc
tejas <jaybalaji192@gmail.com>
2025-09-09 18:20:39 +0530

    refactor: id logic improved

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,8 +2,7 @@ import express, { type Request, type Response } from 'express';
 const app = express();
 import { connectDB } from '@/config/db.js';
 import pasteRouter from '@/routes/paste.route.js';
-import cors from "cors"
-import configurations from '@/config/configurations.js';
+import cors from "cors";
 import logger from '@/config/logger.js';
 
 connectDB();
@@ -16,7 +15,8 @@ app.get('/hello', (req: Request, res: Response) => {
 app.use(cors({
     origin: [
         "https://snipit-rho.vercel.app",
-        "https://snipit-nu.vercel.app"
+        "https://snipit-nu.vercel.app",
+        "http://localhost:5173"
     ],
     credentials: true,
     methods: "GET,POST,PUT,DELETE,OPTIONS",
