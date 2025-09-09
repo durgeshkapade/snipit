2a495ad134b1425ce57346c74f6ba8a8ce0d5a96
durgeshkapade <kapadedurgesh6@gmail.com>
2025-09-09 17:37:53 +0530

    feat: Add the Toast

--- frontend/src/pages/display.tsx
+++ frontend/src/pages/display.tsx
@@ -31,6 +31,8 @@ const DisplayPage = () => {
     loadData();
   }, []);
 
+
+
   const handleDelete = async () => {
     toast("Are you sure you want to delete this paste?", {
       position: "top-center",
@@ -80,6 +82,8 @@ const DisplayPage = () => {
 
     setIsEdit(false);
   };
+
+
 
   return (
     <div className="h-fit max-h-screen">
